c9da5dd22a0b03f062da39b7beeac4b60c13f932
Leshan Patterson <leshan0627@gmail.com>
2021-12-01 21:08:51 +0000

    Updates to code for edge cases

--- timer1.js
+++ timer1.js
@@ -1,6 +1,8 @@
 const arr = process.argv.slice(2);
 for (let num of arr) {
-  setTimeout(() => {
-    process.stdout.write(`\x07 ${num} seconds`);
-  }, num * 1000);
+  if (num > 0 && !isNaN(num)) {
+    setTimeout(() => {
+      process.stdout.write(`\x07 ${num} seconds`);
+    }, num * 1000);
+  }
 }
